aca515fa5a31f8ad10f0373b3722f4f28fb66d53
Jack Baron <jackmbaron@gmail.com>
2021-02-10 14:41:53 +0000

    add notice if loading takes >5s

--- web/components/PlayerLoading.tsx
+++ web/components/PlayerLoading.tsx
@@ -1,18 +1,52 @@
-import { FC } from 'react'
+import clsx from 'clsx'
+import { FC, useEffect, useState } from 'react'
 import Spinner from '~assets/svg/spinner.svg'
 import { PlayerOverlay } from './PlayerOverlay'
 import type { IProps } from './PlayerOverlay'
 
-export const PlayerLoading: FC<IProps> = ({ ...props }) => (
-  <PlayerOverlay {...props}>
-    <style jsx>
-      {`
-        img
-          width 6vw
-          height auto
-      `}
-    </style>
+export const PlayerLoading: FC<IProps> = ({ ...props }) => {
+  const [tooLong, setTooLong] = useState<boolean>(false)
+  useEffect(() => {
+    setTimeout(() => {
+      setTooLong(true)
+    }, 1000 * 5)
+  })
 
-    <img src={Spinner} alt='Loading Spinner' />
-  </PlayerOverlay>
-)
+  return (
+    <PlayerOverlay {...props}>
+      <style jsx>
+        {`
+          div
+            display flex
+            flex-direction column
+            align-items center
+            justify-content center
+
+          img
+            width 6vw
+            height auto
+
+          p
+            $font-size = 1.25rem
+            font-size $font-size
+            height 0
+            opacity 0
+            margin 0
+            transition all 1s ease
+
+            &.visible
+              height $font-size + 0.5rem
+              opacity 1
+              margin-top 2.5rem
+        `}
+      </style>
+
+      <div>
+        <img src={Spinner} alt='Loading Spinner' />
+        <p className={clsx(tooLong && 'visible')}>
+          Loading taking too long? Check your Channel ID.
+        </p>
+      </div>
+    </PlayerOverlay>
+  )
+}
