8e1c32fd442c299bad096b5ce148193a41ba7701
Jack Baron <jackmbaron@gmail.com>
2021-02-10 12:27:26 +0000

    fix janus types

---
+++ web/janus-env.d.ts
@@ -0,0 +1,3 @@
+declare module 'janus-ftl-player' {
+  export * from 'janus-ftl-player/src'
+}
--- web/next.config.js
+++
@@ -1,2 +0,0 @@
-const withTM = require('next-transpile-modules')(['janus-ftl-player'])
-module.exports = withTM();
