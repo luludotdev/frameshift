a4d523ad6b0a8965cc64d8a50eca31a1ebe63185
Jack Baron <jackmbaron@gmail.com>
2021-02-10 20:08:06 +0000

    noindex 404 pages

--- web/pages/404.tsx
+++ web/pages/404.tsx
@@ -1,9 +1,14 @@
 import Error from 'next/error'
+import Head from 'next/head'
 import { FC } from 'react'
 import { ErrorContainer } from '~components/ErrorContainer'
 
 export const Error404: FC = () => (
   <ErrorContainer>
+    <Head>
+      <meta name='robots' content='noindex' />
+    </Head>
+
     <Error statusCode={404} />
   </ErrorContainer>
 )
