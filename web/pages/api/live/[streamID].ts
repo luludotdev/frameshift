99c213b641f191be1943559d4a51f45d8f0f204c
Jack Baron <jackmbaron@gmail.com>
2021-02-15 14:18:12 +0000

    public API route to expose stream info

---
+++ web/pages/api/live/[streamID].ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import nc from 'next-connect'
+import { redis } from '~redis'
+
+const router = nc<NextApiRequest, NextApiResponse>()
+
+router.get(async (request, resp) => {
+  const streamID = Array.isArray(request.query.streamID)
+    ? request.query.streamID[0]
+    : request.query.streamID
+
+  if (Number.isNaN(Number.parseInt(streamID, 10))) {
+    resp.status(400).end()
+  }
+
+  const stream = await redis.hgetall(`ftl:${streamID}`)
+  if (Object.keys(stream).length === 0) {
+    resp.status(404).end()
+    return
+  }
+
+  resp.json(stream)
+})
+
+export default router
