3c1c46b40cff759d2bde5c9140fcace41ccb0140
Jack Baron <jackmbaron@gmail.com>
2021-02-23 12:13:13 +0000

    rework redis keys

--- server/lib/redis/keys.ts
+++ server/lib/redis/keys.ts
@@ -1,9 +1,9 @@
 const PREFIX = 'ftl'
 type KeyFn = (streamID: string) => string
 
-export const dataKey: KeyFn = id => `${PREFIX}:data:${id}`
+export const dataKey: KeyFn = id => `${PREFIX}:${id}:data`
 
-export const previewKey: KeyFn = id => `${PREFIX}:preview:${id}`
+export const previewKey: KeyFn = id => `${PREFIX}:${id}:preview`
 export enum PreviewKeyField {
   Buffer = 'data',
   MIME = 'mime',
