93fa608b1ee44b0c134da7569506e848a587fc76
Jack Baron <jackmbaron@gmail.com>
2021-04-03 20:47:37 +0100

    fix uptime nan

--- server/components/controls/Viewers.tsx
+++ server/components/controls/Viewers.tsx
@@ -4,7 +4,13 @@ import { ControlValue } from './ControlValue'
 
 export const Viewers: FC = () => {
   const { data, error } = useStats()
-  const viewers = error ? '???' : (data?.ingestViewers ?? 0).toLocaleString()
+  const viewers = error
+    ? '???'
+    : data === undefined
+    ? '0'
+    : Number.isNaN(data.ingestViewers)
+    ? '0'
+    : data.ingestViewers.toLocaleString()
 
   return (
     <ControlValue title='Viewers' icon='user'>
