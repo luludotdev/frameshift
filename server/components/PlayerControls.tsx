2a3481eb31c054e8e0d78b6f26936e71b96c3f48
Jack Baron <jackmbaron@gmail.com>
2021-04-03 04:14:22 +0100

    handle hoverover of controls properly

--- server/components/PlayerControls.tsx
+++ server/components/PlayerControls.tsx
@@ -1,52 +1,75 @@
+import { useCallback, useState } from 'react'
 import type { FC } from 'react'
 import type { Except } from 'type-fest'
 import { PlayerOverlay } from './PlayerOverlay'
 import type { IProps } from './PlayerOverlay'
 
 type Props = Except<IProps, 'transparent' | 'fadeDelay' | 'fadeTime'>
-export const PlayerControls: FC<Props> = ({ ...props }) => (
-  <PlayerOverlay transparent fadeTime={0.25} fadeDelay={0} {...props}>
-    <style jsx>
-      {`
-        div.container
-          width 100%
-          height 100%
+export const PlayerControls: FC<Props> = ({ hidden }) => {
+  const [hover, setHover] = useState<boolean>(false)
+  const onHoverOver = useCallback(() => setHover(true), [])
+  const onHoverOut = useCallback(() => setHover(false), [])
 
-          display flex
-          align-items flex-end
+  const actualHidden = !hover && hidden
+  return (
+    <PlayerOverlay
+      transparent
+      fadeTime={0.25}
+      fadeDelay={0}
+      hidden={actualHidden}
+    >
+      <style jsx>
+        {`
+          div.container
+            width 100%
+            height 100%
 
-        div.bar
-          $height = 20px
-          $color = rgba(7, 7, 7, 0.8)
-          $padding = 0.2rem
+            display flex
+            align-items flex-end
 
-          width 100%
-          height $height
-          background-color $color
+          div.bar
+            $height = 20px
+            $color = rgba(7, 7, 7, 0.8)
+            $padding = 0.2rem
 
-          position relative
-          padding 1rem
-          display flex
-          align-items center
-
-          pointer-events initial
-          backdrop-filter blur(2px)
-
-          &::before
-            content ' '
-            position absolute
-            top 0
-            left -100%
-            transform translateY(-100%)
-
-            width 200%
+            width 100%
             height $height
-            background linear-gradient(0deg, $color 0%, transparent 100%);
-      `}
-    </style>
+            background-color $color
 
-    <div className='container'>
-      <div className='bar'>bar along the bottom</div>
-    </div>
-  </PlayerOverlay>
-)
+            position relative
+            padding 1rem
+            display flex
+            align-items center
+
+            backdrop-filter blur(2px)
+
+            &::before
+              content ' '
+              position absolute
+              top 0
+              left -100%
+              transform translateY(-100%)
+
+              width 200%
+              height $height
+              background linear-gradient(0deg, $color 0%, transparent 100%);
+        `}
+      </style>
+
+      <style jsx>
+        {`
+          div.bar
+            pointer-events ${actualHidden ? 'none' : 'initial'}
+        `}
+      </style>
+
+      <div
+        className='container'
+        onMouseOver={onHoverOver}
+        onMouseOut={onHoverOut}
+      >
+        <div className='bar'>bar along the bottom</div>
+      </div>
+    </PlayerOverlay>
+  )
+}
