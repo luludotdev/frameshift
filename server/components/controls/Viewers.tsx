7d49455c62909622d108678528264f498415edf8
Jack Baron <jackmbaron@gmail.com>
2021-04-03 05:20:40 +0100

    super basic viewers control

--- server/components/controls/Viewers.tsx
+++ server/components/controls/Viewers.tsx
@@ -1,3 +1,9 @@
 import type { FC } from 'react'
+import { useStats } from '~hooks/useStats'
 
-export const Viewers: FC = () => <span>viewers</span>
+export const Viewers: FC = () => {
+  const { data, error } = useStats()
+  const viewers = error ? '???' : data?.ingestViewers ?? 0
+
+  return <div>viewers: {viewers}</div>
+}
